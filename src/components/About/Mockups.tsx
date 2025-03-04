edb66dd4bfe742520185b55d04a7ed5f7c2ae11f
fadhilahnurhidayah <12350120165@students.uin-suska.ac.id>
2025-03-05 01:58:22 +0700

    Fixed Mockups component issue3

--- src/components/About/Mockups.tsx
+++ src/components/About/Mockups.tsx
@@ -1,43 +1,44 @@
 "use client"
 
-import React, { useRef } from "react";
+import React, { useRef, ReactNode } from "react";
 import Image from "next/image";
 import AboutOutro1 from '../../../public/images/general/about/aboutOutro1.jpg'
 import AboutOutro2 from '../../../public/images/general/about/aboutOutro2.jpg'
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-function Mockups() {
-  const image1 = useRef(null)
-  const image1Div = useRef(null)
-  const image2Div = useRef(null)
-  const image2 = useRef(null)
+const Mockups: React.FC = () => {
+  const image1 = useRef<HTMLImageElement>(null)
+  const image1Div = useRef<HTMLDivElement>(null)
+  const image2Div = useRef<HTMLDivElement>(null)
+  const image2 = useRef<HTMLImageElement>(null)
 
   useGSAP(() => {
-    gsap.to(image1.current, {
-      y: -40,
-      ease: "power1.inOut",
-      scrollTrigger: {
-        trigger: image1Div.current,
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true
-      }
-    })
+    if (image1.current && image1Div.current && image2.current && image2Div.current) {
+      gsap.to(image1.current, {
+        y: -40,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: image1Div.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true
+        }
+      })
 
-    gsap.to(image2.current, {
-      y: 30,
-      ease: "power1.inOut",
-      scrollTrigger: {
-        trigger: image2Div.current,
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true
-      }
-    })
+      gsap.to(image2.current, {
+        y: 30,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: image2Div.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true
+        }
+      })
+    }
   })
 
-  // Add a return statement with the JSX you want to render
   return (
     <section className="relative flex flex-col md:flex-row gap-10 py-20">
       <div ref={image1Div} className="md:w-1/2">
